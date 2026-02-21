32b28ca32e390dabc504911479fdc756c6ef9697
JEFF <jeffomullo559@gmail.com>
2026-02-21 12:20:54 +0300

    Create MpesaResponse.java file

---
+++ app/src/main/java/com/savingslock/app/models/MpesaResponse.java
@@ -0,0 +1,8 @@
+package com.savingslock.app.models; 
+
+public class MpesaResponse {
+    public boolean success;
+    public String transactionId;
+    public String message;
+    public long timestamp;
+}
